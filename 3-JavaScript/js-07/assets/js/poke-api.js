58168a3d8f0b81d5e40ccb71a790e9326f09fb7d
ARTUROLOPEZROD <a.lr9710al@gmail.com>
2022-08-12 20:32:01 -0500

    poke api y node.js

--- 3-JavaScript/js-07/assets/js/poke-api.js
+++ 3-JavaScript/js-07/assets/js/poke-api.js
@@ -1,7 +1,6 @@
+
 /* URL a consumir */
-const urlPokemon = 'https://pokeapi.co/api/v2/pokemon/123'
-
-
+const urlPokemon = 'https://pokeapi.co/api/v2/pokemon/'
 
 /* elementos del DOM */
 const imgPokemon = document.getElementById('img-pokemon')
@@ -10,36 +9,93 @@ const idPokemon = document.getElementById('id-pokemon')
 
 const nombrePokemon = document.getElementById('nombre-pokemon')
 
+const listaHabilidades = document.getElementById('lista-habilidades')
+
+const listaTipos = document.getElementById('lista-tipos')
+
+const formulario = document.getElementById('buscador-pokemon')
+console.log(formulario);
+
+/* eventos */
+
+formulario.addEventListener('submit', (e) => {  //submit recarga la pagina, (e) inicializa el evento
+
+  e.preventDefault()
+
+const inputPokemon = document.getElementById('busqueda-pokemon')
+console.log(inputPokemon.value);
+
+const nuevaBusqueda = urlPokemon + inputPokemon.value
+console.log(nuevaBusqueda);
 
 
+obtenerPokemon(nuevaBusqueda)
+
+
+})
 /* Funciones */
 
 async function obtenerPokemon(url){
 
-  const respuesta = await fetch(url)
-  const datos = await respuesta.json()
-
-  /* console.log(datos);
-  console.log(datos.forms[0].name);
-  console.log(datos.abilities);
-  console.log(datos.id);
-  console.log(datos.types);
-  console.log(datos.sprites.other['official-artwork'].front_default); */
+  try {
+    const respuesta = await fetch(url)
+    const datos = await respuesta.json()
   
-    const pokemon = {
-      nombre: datos.forms[0].name,
-      habilidades: datos.abilities,
-      id: datos.id,
-      tipos: datos.types,
-      imagen: datos.sprites.other['official-artwork'].front_default
-
+    /* console.log(datos);
+    console.log(datos.forms[0].name);
+    console.log(datos.abilities);
+    console.log(datos.id);
+    console.log(datos.types);
+    console.log(datos.sprites.other['official-artwork'].front_default); */
+    
+      const pokemon = {
+        nombre: datos.forms[0].name,
+        habilidades: datos.abilities,
+        id: datos.id,
+        tipos: datos.types,
+        imagen: datos.sprites.other['official-artwork'].front_default
+  
+      }
+    
+      // imagen, nombre y id
+      imgPokemon.src = pokemon.imagen;
+      idPokemon.textContent = `ID: ${pokemon.id}`;
+      nombrePokemon.textContent = pokemon.nombre;
+  
+      // habilidades             // innerHTML reemplaza el interior con += lo que tiene tambien se agrega
+      
+      let template = ``
+      
+      for(let i=0; i < pokemon.habilidades.length; i++) {
+        
+        const nombreHabilidad = pokemon.habilidades[i].ability.name
+        
+        template += `<li class="list-group-item"> ${nombreHabilidad} </li>`   //${ } interpolar += suma y no sobrescribe
     }
-    imgPokemon.src = pokemon.imagen;
-    idPokemon.textContent = `ID: ${pokemon.id}`;
-    nombrePokemon.textContent = pokemon.nombre;
+      
+      listaHabilidades.innerHTML = template;
+  
+  
+    // tipos
+  
+  
+    let templateTipos = ''
+  
+    pokemon.tipos.forEach((tipo) => {       // forEach reasignacion del nombre
+      const nombreTipo = tipo.type.name
+      
+  
+      templateTipos += `<li class="list-group-item"> ${nombreTipo} </li>`   //${ } interpolar += suma y no sobrescribe
+  
+    })
+    
+    listaTipos.innerHTML = templateTipos
+      
 
+  }catch(e) {
+alert('Pokemon no valido')   // try , contiene el codigo funcional
+                            // catch , para imprimir error al no cumplir el codigo
+  }
 
-    //console.log(pokemon.imagen);
 }
 
-obtenerPokemon(urlPokemon)
--- 3-JavaScript/js-08/clasesoop.js
+++ 3-JavaScript/js-08/clasesoop.js
@@ -23,3 +23,38 @@ console.log(persona1.nombre);
 let persona2 = new Persona('Ana', 'Cruz');  //cada objeto es una copia de la clase Persona, y cada uno tiene sus propios valores
 console.log(persona2);
 
+
+/* */
+
+
+
+
+class Persona {
+    constructor(nombre, apellido){
+        this._nombre = nombre;
+        this._apellido = apellido;
+    }
+
+    get nombre(){
+        return this._nombre;
+    }
+
+    set nombre(nombre){
+        this._nombre = nombre
+    }
+}
+
+let persona1 = new Persona('David','Castellanos');
+
+console.log(persona1);
+
+persona1.nombre = 'David Guzman'
+
+console.log(persona1.nombre);
+
+let persona2 = new Persona ('Arturo', 'Guzman');
+console.log(persona2);
+
+
+
+
