a3433075950126279cbbe99195cadac3eff8f6fb
ARTUROLOPEZROD <a.lr9710al@gmail.com>
2022-08-16 23:29:01 -0500

    Java - JDK

---
+++ java/java-01/prueba2/src/main/java/HolaMundo.java
@@ -0,0 +1,7 @@
+public class HolaMundo {
+    public static void main(String[] args) {
+        //String miVariable = "Hola mundo Java";
+        String nombre = "Arturo";
+        System.out.println("saludos desde java a: " + nombre);
+    }
+}
