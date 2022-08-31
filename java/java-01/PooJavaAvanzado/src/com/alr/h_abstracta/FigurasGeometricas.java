4411340aed97c9d982c8a9527d4ae32d46b7647a
ARTUROLOPEZROD <a.lr9710al@gmail.com>
2022-08-31 03:09:47 -0500

    polimorfismo, herencias y abstraccion

--- java/java-01/PooJavaAvanzado/src/com/alr/h_abstracta/FigurasGeometricas.java
+++ java/java-01/PooJavaAvanzado/src/com/alr/h_abstracta/FigurasGeometricas.java
@@ -1,11 +1,17 @@
 package com.alr.h_abstracta;
 
 public abstract class FigurasGeometricas {
+    private String nombre;
+
     public abstract float area();                                    //metodo abstracto es un metodo vacio
 
+    public FigurasGeometricas(String nombre) {
+        this.nombre = nombre;
+    }
 
-
-
-
+    @Override
+    public String toString() {
+        return nombre + " area -> "+ area();
+    }
 
 }
